975c9e1b659943ae1a9ade5894892ac958903dee
vidyarautela <vidya.rautela.28@gmail.com>
2024-04-04 23:42:28 +0530

    testing contribution

--- src/com/leetcode/inputCharacterRules.java
+++ src/com/leetcode/inputCharacterRules.java
@@ -11,6 +11,8 @@ Write a program that takes a character as input and prints 1, 0, or -1 according
 0, if the character is a lowercase alphabet (a - z).
 -1, if the character is not an alphabet.
 
+
+testing commit
 */
 
 public class inputCharacterRules {
